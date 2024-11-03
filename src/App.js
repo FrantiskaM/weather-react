5632617a5e141f306206bbc040cadbe3cd45b014
FrantiskaM <frantiska.malucka@gmail.com>
2024-11-03 19:59:15 +0100

    Added functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
 import Weather from "./Weather";
-
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
+    <div>
+      <header>
+        <h1>Weather App</h1>
         <Weather city="Tokyo" />
       </header>
+      <p>
+        Coded by
+        <a href="https://github.com/FrantiskaM" target="_blank" rel="noopener">
+          FrantiskaM
+        </a>
+        on
+        <a
+          href="https://github.com/FrantiskaM/weather-react"
+          target="_blank"
+          rel="noopener"
+        >
+          Github
+        </a>
+        , hosted on
+        <a
+          href="https://weather-react-myproject.netlify.app/"
+          target="_blank"
+          rel="noopener"
+        >
+          Netlify
+        </a>
+      </p>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,55 @@
-import React from "react";
+import { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}C`
-    );
-  }
-  let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+export default function Weather() {
+  let [city, setCity] = useState(null);
+  let [results, setResults] = useState({ ready: false });
 
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather!!</h2>;
+  function showWeather(response) {
+    setResults({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidty,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleChange(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=082d3d02ffdb12f2fd9b259e2ced1d0da&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function handleResponse(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div>
+      <form onSubmit={handleChange}>
+        <input type="Search" onChange={handleResponse}></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+
+  if (results.ready) {
+    return (
+      <div>
+        <ul>
+          <li>Temperature: {Math.round(results.temperature)}</li>
+          <li>Description: {results.description}</li>
+          <li>Humidity: {results.humidity}</li>
+          <li>Wind: {results.wind}</li>
+          <li>
+            <img src={results.icon} alt="weather-icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
