246873dee2137621c58cb2f1cad098912df34ab9
FrantiskaM <frantiska.malucka@gmail.com>
2024-11-03 20:10:20 +0100

    Added function

--- src/App.js
+++ src/App.js
@@ -4,27 +4,31 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Weather App</h1>
-        <Weather city="Tokyo" />
+        <h1>My Weather App</h1>
+        <Weather city="Paris" />
       </header>
       <p>
         Coded by
-        <a href="https://github.com/FrantiskaM" target="_blank" rel="noopener">
-          FrantiskaM
+        <a
+          href="https://github.com/FrantiskaM"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frantiska
         </a>
         on
         <a
           href="https://github.com/FrantiskaM/weather-react"
           target="_blank"
-          rel="noopener"
+          rel="noopener noreferrer"
         >
           Github
         </a>
         , hosted on
         <a
-          href="https://weather-react-myproject.netlify.app/"
+          href="https://app.netlify.com/sites/weather-react-myproject/overview"
           target="_blank"
-          rel="noopener"
+          rel="noopener noreferrer"
         >
           Netlify
         </a>
--- src/Weather.js
+++ src/Weather.js
@@ -2,30 +2,28 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
-  let [city, setCity] = useState(null);
-  let [results, setResults] = useState({ ready: false });
+  const [city, setCity] = useState(null);
+  const [results, setResults] = useState({ ready: false });
 
   function showWeather(response) {
     setResults({
       ready: true,
       temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidty,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
-
   function handleChange(event) {
     event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=082d3d02ffdb12f2fd9b259e2ced1d0da&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bf54175800a55e59e6c4d6461deeef12&units=metric`;
+    axios.get(apiUrl).then(showWeather);
   }
 
   function handleResponse(event) {
     setCity(event.target.value);
   }
-
   let form = (
     <div>
       <form onSubmit={handleChange}>
